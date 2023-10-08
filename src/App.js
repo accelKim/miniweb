9eaa346c0929ec3279cfa4585751891cf222bf9e
zy_zero <jiyoungjang0307@gmail.com>
2023-10-09 00:27:45 +0900

    초기 세팅 v2.1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+// 최근 버전일경우 작성해주지 않아도 되지만 구버전에서 돌릴 경우를 생각해서 import
+// import React from "react";
+import { Container } from "react-bootstrap";
+import Layout from "./layouts/Layout";
+
 function App() {
   return (
-    <div>
-      <h1>Main입니다.</h1>
-    </div>
+    <Layout>
+      <Container>App</Container>
+    </Layout>
   );
 }
 
