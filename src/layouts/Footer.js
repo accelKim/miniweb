e78678990ca7eac10b9c781d940c2370017bd130
accelKim <dnjsgh567877@gmail.com>
2023-10-11 17:54:10 +0900

    0.1

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <Container>
+        <div className='footer_info'>
+          <span>찾아오시는길</span>
+          <span>이메일무단수집거부</span>
+          <span>개인정보처리방침</span>
+        </div>
+      </Container>
+    </footer>
   )
 }
 
