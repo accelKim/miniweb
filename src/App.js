e9ce293ae96f307cee713f16db0f803f24ff8dca
zy_zero <jiyoungjang0307@gmail.com>
2023-10-11 15:43:06 +0900

    favicon 적용 완료 및 상단 메뉴 틀 작성 중

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // 최근 버전일경우 작성해주지 않아도 되지만 구버전에서 돌릴 경우를 생각해서 import
-// import React from "react";
+import React from "react";
 import { Container } from "react-bootstrap";
 import Layout from "./layouts/Layout";
 
 function App() {
   return (
     <Layout>
-      <Container>App</Container>
+      <Container style={{ minHeight: '75vh' }}>Main 페이지입니다.</Container>
     </Layout>
   );
 }
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Navbar, Nav, Button, Container } from "react-bootstrap";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand>Ori University</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link>
+                <Button variant="primary">Sign In</Button>
+              </Nav.Link>
+              <Nav.Link>
+                <Button variant="secondary">Sign Up</Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
 
 export default Header;
