3f06a42c00b18f55f63d5548c10e93a57c351da2
accelKim <dnjsgh567877@gmail.com>
2023-10-11 22:19:50 +0900

    0.1

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,17 +1,56 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import './Footer.css';
+
+function Logo() {
+  const LogoImg = {
+    width : 30,
+    height : 50,
+  };
+}
+
 
 const Footer = () => {
   return (
-    <footer>
-      <Container>
-        <div className='footer_info'>
-          <span>찾아오시는길</span>
-          <span>이메일무단수집거부</span>
-          <span>개인정보처리방침</span>
-        </div>
-      </Container>
-    </footer>
+     <footer class="footer">
+  <div class="footer-left col-md-4 col-sm-6">
+    <p class="about">
+      <h2> 오리대학교</h2>오리대학교는 평생교육법에 의한 전공대학입니다.
+    </p>
+    <div class="icons">
+      <a href="#"><i class="fa fa-facebook"></i></a>
+      <a href="#"><i class="fa fa-twitter"></i></a>
+      <a href="#"><i class="fa fa-linkedin"></i></a>
+      <a href="#"><i class="fa fa-google-plus"></i></a>
+      <a href="#"><i class="fa fa-instagram"></i></a>
+    </div>
+  </div>
+  <div class="footer-center col-md-4 col-sm-6">
+    <div>
+      <i class="fa fa-map-marker"></i>
+      <p><span>경기도 성남시 분당구 성남대로 34</span>하나프라자 6층</p>
+    </div>
+    <div>
+      <i class="fa fa-phone"></i>
+      <p>0507-1309-9319</p>
+    </div>
+    <div>
+      <i class="fa fa-envelope"></i>
+      <p><a href="#">kosta01@kosta.com</a></p>
+    </div>
+  </div>
+  <div class="footer-right col-md-4 col-sm-6">
+  <img class="footer-logo" alt='logo' src="/img/logo.png" style={{width: 200,height: 130}} />
+    <p class="menu">
+      <a href="#">  관련사이트</a> | 
+      <a href="#">  입학안내</a> | 
+      <a href="#">  원격교육</a> | 
+      <a href="#">  종합정보시스템</a> | 
+      <a href="#">  학술정보관</a> | 
+      <a href="#">  커리어매니지먼트센터</a>
+    </p>
+    <p class="name">ⓒ Since 2023 Ori College, All Rights Reserved.</p>
+  </div>
+</footer>
   )
 }
 
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React from "react";
+import { Navbar, Nav, Button, Container } from "react-bootstrap";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">오리대학교 Ori University</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#intro">학교소개</Nav.Link>
+            <Nav.Link href="#undergraduate_major">학부전공</Nav.Link>
+            <Nav.Link href="#academic_info">학사정보</Nav.Link>
+            <Nav.Link href="#class_grade">수업•성적</Nav.Link>
+            <Nav.Link href="#university life">대학생활</Nav.Link>
+            <Nav.Link href="#enrolment">수강신청</Nav.Link>
+          </Nav>
+          <Nav className="ms-auto">
+            <Nav.Link href="#login">
+              <Button variant="secondary">Log In</Button>
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
 
 export default Header;
