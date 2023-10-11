bf7e8eddf31ba63fd89b64bd1a0810c039b8c2af
zy_zero <jiyoungjang0307@gmail.com>
2023-10-11 21:51:41 +0900

    상단메뉴 수정 v1.0

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -4,25 +4,23 @@ import { Navbar, Nav, Button, Container } from "react-bootstrap";
 const Header = () => {
   return (
     <header>
-      <Navbar bg="primary" data-bs-theme="dark">
+      <Navbar bg="primary" data-bs-theme="dark" style={{ height: '80px' }}>
         <Container>
-          <Nav className="me-auto">
           <Navbar.Brand href="#home">
             {/* 여기에 로고 이미지를 추가 */}
             <img
               src="ori-logo.png"
               width="160"
               height="102"
-              className=""
+              alt="ori-logo"
             />
           </Navbar.Brand>
-          </Nav>
           <Nav className="me-auto">
             <Nav.Link href="#intro">학교소개</Nav.Link>
             <Nav.Link href="#undergraduate_major">학부전공</Nav.Link>
             <Nav.Link href="#academic_info">학사정보</Nav.Link>
             <Nav.Link href="#class_grade">수업•성적</Nav.Link>
-            <Nav.Link href="#university life">대학생활</Nav.Link>
+            <Nav.Link href="#university_life">대학생활</Nav.Link>
             <Nav.Link href="#enrolment">수강신청</Nav.Link>
           </Nav>
           <Nav className="ms-auto">
