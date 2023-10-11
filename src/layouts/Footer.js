1d959b41e5cff08734bfc2391f6fb8e23e29ea2f
zy_zero <jiyoungjang0307@gmail.com>
2023-10-11 17:33:18 +0900

    상단 메뉴 틀 완성(아코디언 X)

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <Container>
+        <div style={{textAlign:"left"}}>
+          찾아오시는 길 
+        </div>
+      </Container>
+    </footer>
   )
 }
 
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -4,17 +4,22 @@ import { Navbar, Nav, Button, Container } from "react-bootstrap";
 const Header = () => {
   return (
     <header>
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="primary" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand>Ori University</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Nav.Link>
-                <Button variant="primary">Log In</Button>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+          <Navbar.Brand href="#home">오리대학교 Ori University</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#intro">학교소개</Nav.Link>
+            <Nav.Link href="#undergraduate_major">학부전공</Nav.Link>
+            <Nav.Link href="#academic_info">학사정보</Nav.Link>
+            <Nav.Link href="#class_grade">수업•성적</Nav.Link>
+            <Nav.Link href="#university life">대학생활</Nav.Link>
+            <Nav.Link href="#enrolment">수강신청</Nav.Link>
+          </Nav>
+          <Nav className="ms-auto">
+            <Nav.Link href="#login">
+              <Button variant="secondary">Log In</Button>
+            </Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
     </header>
