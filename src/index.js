2d2cedad89f9045c3af5889ce70d1e166bd99ca2
accelKim <dnjsgh567877@gmail.com>
2023-10-12 14:24:49 +0900

    v0.12

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './bootstrap.min.css';
 import App from './App';
 //import Notice from './notice'
 import Schedule from './schedule';
+import Calendar from './calendar';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Schedule />
+    <Calendar />
   </React.StrictMode>
 );
