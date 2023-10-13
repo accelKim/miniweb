b91cf5851d56cdeabe02d5af938a7c72d9529276
zy_zero <jiyoungjang0307@gmail.com>
2023-10-14 02:13:24 +0900

    feat: Home에서 notice import 코드 수정

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Slide from '../main/Slide';
-import Notice from "../main/Notice";
+import Notice from '../main/notice';
 import Schedule from '../main/Schedule';
 import QuickUnderGraduate from '../main/QuickUnderGraduate';
 
