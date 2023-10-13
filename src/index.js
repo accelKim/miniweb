d7d463f512aae309976bccd5ce4d2623fb603f91
zy_zero <jiyoungjang0307@gmail.com>
2023-10-13 10:26:25 +0900

    feat: 수정1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/main.css';
+import './css/Header.css'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import '../css/main.css';
+import '../css/Header.css';
 
 function Header() {
     return (
